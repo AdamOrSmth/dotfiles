55cca5a6777f055fc4e01db2ff0ec00d64430b3f
Ad <me@adamorsomething.xyz>
2022-05-29 21:40:47 -0400

    Generate hosts list automatically

--- flake.nix
+++ flake.nix
@@ -5,16 +5,22 @@
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
-      system = "x86_64-linux";
-      commonConfig = [ (import ./overlay.nix) ];
-      hosts = [ "AdPC" "AdLaptop" ];
-      modules = [ ];
       inherit (nixpkgs) lib;
+      system = "x86_64-linux";
+      modules = [ ];
+      hosts = lib.mapAttrsToList (n: _: lib.removeSuffix ".nix" n)
+        (builtins.readDir ./hosts);
     in {
-      nixosConfigurations = lib.attrsets.genAttrs hosts (host:
-        lib.nixosSystem {
+      nixosConfigurations = lib.genAttrs hosts (host:
+        let
+          common = [
+            (import ./overlay.nix)
+            # Hostname should always be the same
+            ({ ... }: { networking.hostName = host; })
+          ];
+        in lib.nixosSystem {
           inherit system;
-          modules = commonConfig ++ modules
+          modules = common ++ modules
             ++ lib.singleton (import ./hosts/${host}.nix);
         });
     };
