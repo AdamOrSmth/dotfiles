d9706888388c882dec3913c254ab137ac337f373
Ad <me@adamorsomething.xyz>
2022-05-30 11:12:53 -0400

    Clean up flake + overlay, import functions better

--- flake.nix
+++ flake.nix
@@ -1,37 +1,62 @@
 {
   description = "My NixOS configuration";
 
-  inputs = { nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; }; };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
-      # Bootstrap custom library functions here, since the overlay
-      # isn't loaded until later
-      lib = nixpkgs.lib.extend (self: super: { my = import ./lib.nix self; });
-      inherit (lib.my) mapModules mapModulesRec mapModulesRec';
       system = "x86_64-linux";
-      hosts = builtins.attrNames (mapModules ./hosts lib.id);
+
+      # Bootstrap custom library functions. Why is documentation for `extend` so hard to find?
+      # Actually, that describes Nix in a nutshell. Y'know it's bad when the best example usage
+      # I found of this method was in hlissner's dotfiles, which I basically stole the entire
+      # line from.
+      lib = nixpkgs.lib.extend (self: super: { my = import ./lib.nix self; });
+      inherit (lib) id genAttrs singleton;
+      inherit (lib.my) mapModules mapModulesRec mapModulesRec';
+      inherit (builtins) attrNames split;
+
+      pkgs = import nixpkgs {
+        inherit system;
+        # Add custom library functions here, there's probably a better way and place
+        # to do this, but I'm not about to turn my dotfiles into an
+        # over-engineered setup like hlissner, even though I kinda already
+        # have. Just keep telling yourself that Ad, and eventually you'll have a config
+        # that's just as monkey-patched and hacky as before! Who needs clean code, as long
+        # as I understand the mess, it's fine right? Humans clearly never forget anything!
+        overlays = singleton (self: super: { inherit lib; })
+          ++ import ./overlay.nix;
+      };
+
+      hosts = attrNames (mapModules ./hosts id);
+
       # Import each module, passing it its relative path,
       # which is used to define options. Engineering completely over-complicated solutions
       # for not-very-problematic problems is great, isn't it? Automation go brr.
       # There's probably a better way to do this, but I'm working on this way too late at night,
       # so I really don't feel like finding it.
       modules = mapModulesRec' ./modules (p:
-        let inherit (lib) drop flatten removeSuffix;
-        in import p ([ "my" ]
-          ++ (drop 4 (flatten (builtins.split "/" (removeSuffix ".nix" p))))));
+        let inherit (lib) pipe drop flatten removeSuffix concat;
+        in import p (pipe p [
+          (removeSuffix ".nix")
+          (split "/")
+          flatten
+          (drop 4)
+          (concat [ "my" ])
+        ]));
+
     in {
-      nixosConfigurations = lib.genAttrs hosts (host:
+      nixosConfigurations = genAttrs hosts (host:
         let
           common = [
-            (import ./overlay.nix)
-            # Hostname should always be the same
+            # Hostname should always be the same, so it's only bootstrapped once during
+            # initial setup.
             ({ ... }: { networking.hostName = host; })
           ];
         in lib.nixosSystem {
           inherit system;
-          modules = common ++ modules
-            ++ lib.singleton (import ./hosts/${host}.nix);
+          specialArgs = { inherit lib pkgs; };
+          modules = common ++ modules ++ singleton (import ./hosts/${host}.nix);
         });
     };
 }
--- overlay.nix
+++ overlay.nix
@@ -1,18 +1,7 @@
-# Add all overlays
-{ config, lib, pkgs, ... }:
-
-let
-  inherit (builtins) readDir;
-  inherit (lib) mapAttrs' nameValuePair removeSuffix filterAttrs hasSuffix;
-in {
-  nixpkgs.overlays = [
-    # Custom library functions
-    (self: super: { lib = super.lib // { my = import ./lib.nix super.lib; }; })
-
-    # Custom derivations
-    # Generate a list of files in `packages` sub-directory, filter to only .nix files,
-    # and then add each one to the overlay
-    (self: super:
-      super.lib.my.mapModules ./packages (p: super.callPackage p { }))
-  ];
-}
+# List of all overlays to import in `flake.nix`
+[
+  # Custom derivations
+  # Generate a list of files in `packages` sub-directory, filter to only .nix files,
+  # and then add each one to the overlay
+  (self: super: super.lib.my.mapModules ./packages (p: super.callPackage p { }))
+]
