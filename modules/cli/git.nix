5728f3b0d4972d37ba67a5fd26ede72cf0317897
Ad <me@adamorsomething.xyz>
2022-06-05 09:39:28 -0400

    Add Git configuration

---
+++ modules/cli/git.nix
@@ -0,0 +1,17 @@
+path:
+
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib)
+    getAttrFromPath setAttrByPath mkEnableOption mkOption types mkIf mkMerge;
+  cfg = getAttrFromPath path config;
+  inherit (config.my) configDir;
+in {
+  options = setAttrByPath path { enable = mkEnableOption "Git configuration"; };
+
+  config = mkIf cfg.enable (mkMerge [{
+    environment.systemPackages = builtins.attrValues { inherit (pkgs) git; };
+    my.home.configFiles."git/config".source = "${configDir}/git/config";
+  }]);
+}
