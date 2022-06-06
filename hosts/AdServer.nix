49bc3f1095aca2bd9a238c19ca077b9bafbc8859
Ad <me@adamorsomething.xyz>
2022-06-06 16:35:23 -0400

    Add AdServer configuration

---
+++ hosts/AdServer.nix
@@ -0,0 +1,60 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Options for my various modules
+  my = {
+    cli.nix = {
+      autoGc = true;
+      autoOptimise = true;
+    };
+    hardware.networking = { interface = "enp1s0"; };
+    services = {
+      caddy.enable = true;
+      gitea.enable = true;
+      navidrome = {
+        enable = true;
+        musicFolder = "${config.my.services.syncthing.dataDir}/Music";
+      };
+      openssh = {
+        enable = true;
+        key =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIODhsvZT2xbdvgUOZn12BH1wy8slQAR2kRibdHy164bh <ssh://ad@5.161.48.126|ed25519>";
+      };
+      syncthing.enable = true;
+    };
+  };
+
+  # Custom per-host configuration options
+  deployment.targetHost = "5.161.48.126";
+
+  # Hardware configuration options
+  # (copy from /etc/nixos/hardware-configuration.nix)
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+
+  boot.initrd.availableKernelModules =
+    [ "ahci" "xhci_pci" "virtio_pci" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/e3a3ba17-847f-4790-a373-b53ca933f477";
+    fsType = "ext4";
+  };
+
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/cf262e63-b229-4c89-b97a-91c05d433177"; }];
+
+  hardware.cpu.amd.updateMicrocode = true;
+
+  # State version (copy from auto-generated configuration.nix during install)
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11";
+}
