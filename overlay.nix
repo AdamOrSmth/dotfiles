cddd6ef570da2609cf5900b798610d187906d945
Ad <me@adamorsomething.xyz>
2022-06-13 11:11:26 -0400

    Remove obselete overlay.nix

--- overlay.nix
+++
@@ -1,7 +0,0 @@
-# List of all overlays to import in `flake.nix`
-[
-  # Custom derivations
-  # Generate a list of files in `packages` sub-directory, filter to only .nix files,
-  # and then add each one to the overlay
-  (self: super: super.lib.my.mapModules ./packages (p: super.callPackage p { }))
-]
