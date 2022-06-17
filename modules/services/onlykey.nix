bd4edf80b51dd5ac3015bd62b134fd3d16877c07
Ad <me@adamorsomething.xyz>
2022-06-17 13:30:54 -0400

    Remove custom udev rules

--- modules/services/onlykey.nix
+++ modules/services/onlykey.nix
@@ -16,21 +16,7 @@ in {
   };
 
   config = mkIf cfg.enable {
-    # We make the rules ourselves instead of using the ones in nixpkgs
-    # for full customization
-    # See https://raw.githubusercontent.com/trustcrypto/trustcrypto.github.io/pages/49-onlykey.rules
-    services.udev = {
-      # Required for shebang and whatnot to work
-      path = [ bash ];
-      extraRules = ''
-        ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", ENV{ID_MM_DEVICE_IGNORE}="1"
-        ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", ENV{MTP_NO_PROBE}="1"
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", OWNER:="${username}"
-
-        ACTION=="add", SUBSYSTEMS=="usb", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", MODE:="0660", OWNER:="${username}", RUN+="${binDir}/ok-init"
-        ACTION=="add", KERNEL=="ttyACM*", ATTRS{idVendor}=="1d50", ATTRS{idProduct}=="60fc", MODE:="0660", OWNER:="${username}", RUN+="${binDir}/ok-init"
-      '';
-    };
+    hardware.onlykey.enable = true;
 
     environment = {
       systemPackages = [ onlykey-bin onlykey-cli onlykey-agent libfido2 gnupg ];
