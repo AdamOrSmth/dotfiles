a3ed672fbe05ca560158ede78c052e36f7dfcd63
Ad <me@adamorsomething.xyz>
2022-06-22 09:05:02 -0400

    Fix binary cache for emacs-overlay

--- modules/desktop/emacs.nix
+++ modules/desktop/emacs.nix
@@ -12,9 +12,13 @@ in {
     setAttrByPath path { enable = mkEnableOption "Doom Emacs as editor"; };
 
   config = mkIf cfg.enable (mkMerge [{
-
     # Add binary cache for emacs-overlay
-    nix.settings.substituters = [ "https://nix-community.cachix.org" ];
+    nix = {
+      binaryCaches = [ "https://nix-community.cachix.org" ];
+      binaryCachePublicKeys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
 
     # Enable Emacs daemon
     services.emacs = {
