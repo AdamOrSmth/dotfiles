d726b3f5b15a96d3b42b2cfb72685cb4d39bdcc5
Ad <me@adamorsomething.xyz>
2022-07-05 12:45:13 -0400

    feat(doom): Basic org-roam refactoring
    Still have to fix up capture templates, but this should work (which
    means it definitely won't).

--- config/doom/config.el
+++ config/doom/config.el
@@ -180,88 +180,77 @@
   (setq org-roam-directory org-directory
         org-roam-dailies-directory (expand-file-name "journal/" org-roam-directory)
         org-roam-db-location (expand-file-name ".org-roam.db" org-roam-directory)
-        +org-roam-open-buffer-on-find-file nil
-        ;; Capture templates.
-        org-roam-capture-templates
-        `(("z" "zettel" plain
-           (file ,(expand-file-name "template/note.org" org-directory))
-           :target (file "zettel/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("w" "work")
-          ("ww" "default" plain
-           (file ,(expand-file-name "template/document.org" org-directory))
-           :target (file "work/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("wl" "lab report" plain
-           (file ,(expand-file-name "template/aet-lab-report.org" org-directory))
-           :target (file "work/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t))
-        org-roam-dailies-capture-templates
-        `(("d" "default" entry
-           (file "template/journal.org")
-           :target (file+head
-                    "%<%Y-%m-%d>.org"
-                    "#+title: %<%Y-%m-%d %a>\n\n")
-           :clock-in
-           :clock-resume)))
-  ;; Icons for my sub-directory file "types".
-  (defvar ad/org-roam-icons
-    '(("zettel"  . "📑")
-      ("work"    . "✏")
-      ("journal" . "📖")))
-  ;; Add type prefix to 'org-roam-find-file'.
-  (cl-defmethod org-roam-node-doom-prefix ((node org-roam-node))
-    (cdr (assoc (org-roam-node-doom-type node)
-                ad/org-roam-icons)))
-  (setq org-roam-node-display-template #("${doom-prefix} ${doom-hierarchy:*} ${todo:8} ${doom-type:12} ${doom-tags:24}" 20 35
-                                         (face font-lock-keyword-face)
-                                         36 51
-                                         (face org-tag)))
-  ;; Update zettel file names when title changes to new slug.
-  (defun ad/update-roam-filename ()
-    (interactive)
-    (when (and (org-roam-file-p) ; Ensure it's a roam file of the 'zettel' or 'work' type.
-               (or (string-equal (expand-file-name "zettel/" org-directory) (file-name-directory buffer-file-name))
-                   (string-equal (expand-file-name "work/"   org-directory) (file-name-directory buffer-file-name))))
-      (let
-          ((file-location ; Location that file should be at.
-            (concat
-             (file-name-directory buffer-file-name)
-             (s-replace-regexp "^\\([0-9]\\{14\\}\\).*" "\\1" (file-name-base buffer-file-name))
-             "-"
-             (-> (org-roam-node-at-point)
-                 (org-roam-node-file-title)
-                 (org-roam-node-from-title-or-alias)
-                 (org-roam-node-slug))
-             ".org")))
-        (unless (string-equal buffer-file-name file-location)
-          (doom/move-this-file file-location)))))
-  (add-hook 'before-save-hook #'ad/update-roam-filename)
-  ;; Org-Roam-UI
-  (use-package! org-roam-ui
-    :bind (:map doom-leader-notes-map ("r u" . org-roam-ui-mode))
-    :requires (org-roam)
-    :init
-    (use-package! websocket)
-    :custom
-    (org-roam-ui-sync-theme t)
-    (org-roam-ui-follow t)
-    (org-roam-ui-update-on-save t)
-    (org-roam-ui-open-on-start nil))
-  ;; Keybind to visit a random node, excluding dailies.
-  (defun ad/org-roam-random-excluding-dailies (&optional other-window)
-    "Visit a random `org-roam` node, excluding dailies."
-    (interactive "P")
-    (org-roam-node-random
-     other-window
-     (lambda (node)
-       (->> node
-            (org-roam-node-file)
-            (file-name-directory)
-            (string-equal org-roam-dailies-directory)
-            (not)))))
-  (map! (:leader
-         :desc "Random node (no dailies)" "n r A" #'ad/org-roam-random-excluding-dailies)))
+        +org-roam-open-buffer-on-find-file nil))
+
+(defvar ad/org-roam-icons
+  '(("gtd"     . "🗓")
+    ("journal" . "📖")
+    ("log"     . "🗄")
+    ("ref"     . "📚")
+    ("work"    . "✏")
+    ("zettel"  . "💭")))
+
+(cl-defmethod org-roam-node-doom-prefix ((node org-roam-node))
+  (cdr (assoc (org-roam-node-doom-type node)
+              ad/org-roam-icons)))
+(setq org-roam-node-display-template #("${doom-prefix} ${doom-hierarchy:*} ${todo:8} ${doom-type:12} ${doom-tags:24}" 20 35
+                                       (face font-lock-keyword-face)
+                                       36 51
+                                       (face org-tag)))
+
+(defun ad/update-roam-filename ()
+  (interactive)
+  (when (and (org-roam-file-p)
+             (-contains-p '("log" "ref" "work" "zettel") (f-filename (f-parent buffer-file-name))))
+    (let
+        ((new-file-location
+          (concat
+           (file-name-directory buffer-file-name)
+           (s-replace-regexp "^\\([0-9]\\{14\\}\\).*" "\\1" (file-name-base buffer-file-name))
+           "-"
+           (-> (org-roam-node-at-point)
+               (org-roam-node-file-title)
+               (org-roam-node-from-title-or-alias)
+               (org-roam-node-slug))
+           ".org")))
+      (unless (string-equal buffer-file-name new-file-location)
+        (doom/move-this-file new-file-location)))))
+(add-hook 'before-save-hook #'ad/update-roam-filename)
+
+(setq org-roam-capture-templates
+      `(("z" "zettel" plain
+         (file ,(expand-file-name "template/note.org" org-directory))
+         :target (file "zettel/%<%Y%m%d%H%M%S>-${slug}.org")
+         :unnarrowed t)
+        ("w" "work")
+        ("ww" "default" plain
+         (file ,(expand-file-name "template/document.org" org-directory))
+         :target (file "work/%<%Y%m%d%H%M%S>-${slug}.org")
+         :unnarrowed t)
+        ("wl" "lab report" plain
+         (file ,(expand-file-name "template/aet-lab-report.org" org-directory))
+         :target (file "work/%<%Y%m%d%H%M%S>-${slug}.org")
+         :unnarrowed t)))
+
+(setq org-roam-dailies-capture-templates
+      `(("d" "default" entry
+         (file "template/journal.org")
+         :target (file+head
+                  "%<%Y-%m-%d>.org"
+                  "#+title: %<%Y-%m-%d %a>\n\n")
+         :clock-in
+         :clock-resume)))
+
+(use-package! org-roam-ui
+  :bind (:map doom-leader-notes-map ("r u" . org-roam-ui-mode))
+  :requires (org-roam)
+  :init
+  (use-package! websocket)
+  :custom
+  (org-roam-ui-sync-theme t)
+  (org-roam-ui-follow t)
+  (org-roam-ui-update-on-save t)
+  (org-roam-ui-open-on-start nil))
 
 (use-package! org-auto-tangle
   :hook (org-mode . org-auto-tangle-mode))
