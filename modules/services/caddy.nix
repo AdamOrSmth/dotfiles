851f9ac4977e3368727ea94115d2c7961c21874b
Ad <me@adamorsomething.xyz>
2022-07-23 16:56:09 -0400

    feat(caddy): Add cgi plugin

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -2,7 +2,24 @@ path:
 
 { config, lib, pkgs, ... }:
 
-let inherit (lib) setAttrByPath mkEnableOption getAttrFromPath mkIf;
+let
+  inherit (lib) setAttrByPath mkEnableOption getAttrFromPath mkIf;
+  inherit (pkgs) buildGoModule caddy fetchFromGitHub;
+  custom-caddy = let version = "2.5.2";
+  in buildGoModule rec {
+    pname = "caddy-custom";
+
+    inherit version;
+
+    src = fetchFromGitHub {
+      owner = "AdamOrSmth";
+      repo = "caddy";
+      rev = "05e75016024098b2628cc572e9fc613e560de2a8";
+      sha256 = "slMvBkTSm1E8o+CdXy1ZU8YMmrPXiVbZBhjb13w1Mrw=";
+    };
+
+    vendorSha256 = "cu8AcPAk18LcZwK+KGoqH6IjB4/BUg2ZnCQSB691ieQ=";
+  };
 in {
   options =
     setAttrByPath path { enable = mkEnableOption "Caddy reverse-proxying"; };
@@ -10,6 +27,10 @@ in {
   config = mkIf (getAttrFromPath path config).enable {
     services.caddy = {
       enable = true;
+      package = custom-caddy;
+      globalConfig = ''
+        order cgi before file_server
+      '';
       extraConfig = let cfg = config.services;
       in ''
         music.adamorsomething.xyz {
