e9260217731583896881d558673c078bf3738974
Ad <me@adamorsomething.xyz>
2022-07-23 14:47:46 -0400

    fix(git): Proper Gitolite config

--- modules/services/gitolite-cgit.nix
+++ modules/services/gitolite-cgit.nix
@@ -9,6 +9,12 @@ in {
   };
 
   config = mkIf (getAttrFromPath path config).enable {
-    services.gitolite = { enable = true; };
+    services.gitolite = {
+      enable = true;
+      user = "git";
+      group = "git";
+      adminPubkey =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEBf93IkfW57z/aCDd+elE5nQ7LamVVDVAFr3n4oWPzj <ssh://git@git.adamorsomething.xyz|ed25519>";
+    };
   };
 }
