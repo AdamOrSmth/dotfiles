f54282a994cc75f38aa30eb6fab1816377357215
Ad <me@adamorsomething.xyz>
2022-07-23 22:48:18 -0400

    feat(cgit): Add configuration file

--- modules/services/gitolite-cgit.nix
+++ modules/services/gitolite-cgit.nix
@@ -16,5 +16,11 @@ in {
       adminPubkey =
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEBf93IkfW57z/aCDd+elE5nQ7LamVVDVAFr3n4oWPzj <ssh://git@git.adamorsomething.xyz|ed25519>";
     };
+
+    environment.etc.cgitrc.text = ''
+      scan-path=/var/lib/gitolite/repositories
+
+      root-title=Ad's Repos
+    '';
   };
 }
