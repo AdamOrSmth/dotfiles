94e349c2019c402c1fc166ee00148f4d31d9fa1d
Ad <me@adamorsomething.xyz>
2022-09-15 18:16:34 -0400

    bump(webcord): AppImage version

--- packages/webcord.nix
+++ packages/webcord.nix
@@ -1,6 +1,6 @@
 { lib, appimageTools, fetchurl, }:
 
-let version = "3.8.2";
+let version = "3.8.3";
 in appimageTools.wrapType2 {
   pname = "webcord";
   inherit version;
@@ -8,6 +8,6 @@ in appimageTools.wrapType2 {
   src = fetchurl {
     url =
       "https://github.com/SpacingBat3/WebCord/releases/download/v${version}/WebCord-${version}-x64.AppImage";
-    sha256 = "1h59cv9b543d9wdmsr6ns1i2fz2mrhwxzaxfp0dacsgg62lc40fz";
+    sha256 = "05xw6s0l99lzbvrpx8c76c1zckm1qljxqz1rsfp2m1nsq9j2xafa";
   };
 }
