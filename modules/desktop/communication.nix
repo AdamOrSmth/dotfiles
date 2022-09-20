861fb8faaace66da9629b231974c7b2a4503ed39
Ad <me@adamorsomething.xyz>
2022-09-19 21:54:59 -0400

    feat: remove discord + add nord webcord theme

--- modules/desktop/communication.nix
+++ modules/desktop/communication.nix
@@ -7,30 +7,19 @@ let
     getAttrFromPath setAttrByPath mkEnableOption mkOption types mkIf mkMerge;
   cfg = getAttrFromPath path config;
   inherit (pkgs) discord firejail;
-  configDir = "${config.my.configDir}/betterdiscord";
+  configDir = config.my.configDir;
 in {
   options = setAttrByPath path {
     enable = mkEnableOption "chat and communication programs";
   };
 
   config = mkIf cfg.enable (mkMerge [{
-    # Wrap Discord, since it's proprietary
-    # TODO Create a separate Firejail module
-    programs.firejail = {
-      enable = true;
-      wrappedBinaries = {
-        discord = {
-          executable = "${discord}/bin/discord";
-          profile = "${firejail}/etc/firejail/discord.profile";
-        };
-      };
-    };
     environment.systemPackages = builtins.attrValues {
       inherit (pkgs) element-desktop session-desktop-appimage signal-desktop;
       inherit (pkgs.my) webcord;
     };
 
-    # TODO Separate Discord into a separate module
-    my.home.configFiles."BetterDiscord/themes".source = "${configDir}/themes";
+    my.home.configFiles."WebCord/Themes/nord".source =
+      "${configDir}/webcord/nord.css";
   }]);
 }
