a5ea9c93bf60ced8f1df3b642a50ca9aa54dba0c
Ad <me@adamorsomething.xyz>
2022-09-25 16:43:47 -0400

    feat(webcord): bump + screenshare fix

--- packages/webcord.nix
+++ packages/webcord.nix
@@ -1,13 +1,19 @@
 { lib, appimageTools, fetchurl, }:
 
-let version = "3.8.3";
+let version = "3.8.4";
 in appimageTools.wrapType2 {
   pname = "webcord";
   inherit version;
 
+  # Executable name
+  name = "webcord";
+
   src = fetchurl {
     url =
       "https://github.com/SpacingBat3/WebCord/releases/download/v${version}/WebCord-${version}-x64.AppImage";
-    sha256 = "05xw6s0l99lzbvrpx8c76c1zckm1qljxqz1rsfp2m1nsq9j2xafa";
+    sha256 = "1v31n67221idj5bcw4yrchwq3kl78pkawyms4n5qxh4hqf6jbfwi";
   };
+
+  # For screensharing
+  extraPkgs = pkgs: [ pkgs.pipewire ];
 }
